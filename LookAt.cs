b3a324547fa18bdd12ec1acb868127c92806ceea
Marowins <gamemaro@naver.com>
2018-01-29 22:31:48 +0900

    LookAt Function

---
+++ LookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+	Transform obj = null;
+
+	void Start(){
+		// find
+		obj = GameObject.Find("Cube2").transform;
+	}
+
+	void Update(){
+
+		// 죤나 꿀잼, 점프해도 한 옵젝만 바라봄, 석상 눈동자에 사용 가능할듯
+		transform.LookAt (obj);
+	}
+
+}
