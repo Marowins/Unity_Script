c4954a5fc094c3e63b6e46e15b60b68f02f4041a
Marowins <gamemaro@naver.com>
2018-01-29 22:45:25 +0900

    Not use rigidbody gravity, only transform values use

---
+++ transformJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformJump : MonoBehaviour {
+
+	float gravity = 0.0f;
+	Vector3 velocity;
+
+	void Start() {
+		velocity = transform.position;
+	}
+
+	void Update() {
+		if(Input.GetButtonDown("Jump"))
+			gravity = 10.0f;
+
+		velocity.y += gravity * Time.deltaTime;
+		transform.position = velocity;
+
+		gravity -= 0.5f;
+
+		if (velocity.y < 0.5f) {
+			velocity.y = 0.5f;
+			gravity = 0.0f;
+		}
+		
+	}
+
+}
