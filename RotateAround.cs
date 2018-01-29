178c9532991d359a4f26b9a3995ba71d180e879f
Marowins <gamemaro@naver.com>
2018-01-29 22:32:06 +0900

    RotateAround Function

---
+++ RotateAround.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+	Transform obj = null;
+
+	void Start() {
+		// find
+		obj = GameObject.Find("Cube2").transform;
+	}
+
+	void Update(){
+		transform.RotateAround (obj.position, Vector3.up, 40 * Time.deltaTime);
+		transform.LookAt (obj);
+	}
+}
