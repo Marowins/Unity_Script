e82fd1e4b2862a27cc0843e2e12b00945337810b
Marowins <gamemaro@naver.com>
2018-01-29 22:32:58 +0900

    LocalRotation -> World Rotation

---
+++ rotationReset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationReset : MonoBehaviour {
+
+	void Update() {
+		
+		if(Input.GetButtonDown("Fire1")) // 실행조건
+			transform.localRotation = Quaternion.identity; // 월드 or 부모 좌표에 정렬
+
+	}
+
+}
