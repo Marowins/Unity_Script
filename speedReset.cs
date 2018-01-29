4e114fb9f500bc51e5d2b774c1f6c107c2b2049d
Marowins <gamemaro@naver.com>
2018-01-29 22:44:04 +0900

    velocity setting

---
+++ speedReset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedReset : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetButtonDown("Fire1")) // 실행조건
+			GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+			// 현재 속력을 0으로 바꾸는 행위, AddForce종간에 멈추는게 가능
+			//	그러나 유니티 메뉴얼에서는 velocity값을 직젒수정하면 현실적이지 않다고 고치지 말걸 권유
+	}
+}
